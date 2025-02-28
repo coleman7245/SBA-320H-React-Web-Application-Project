83afa85002fdfa9cc3c895613008a3795e741f40
coleman7245 <dlcoleman1@live.com>
2025-02-28 13:17:54 -0700

    Added redux toolkit.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import {Link} from 'react-douter-dom';
+
+function Navbar({url}) {
+    <Link for='/'>
+        <button>
+            Home
+        </button>
+    </Link>
+}
+
+export default Navbar;
