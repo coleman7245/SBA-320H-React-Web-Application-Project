9296dac9b39d4122a14b31fc2fd0cc91da9364bd
coleman7245 <dlcoleman1@live.com>
2025-03-03 09:03:39 -0700

    Added input handling.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import '../styles/Form.css';
 
 function Form({className='Form'}) {
+    const [city, setCity] = useState('');
+    const [country, setCountry] = useState('');
+
+    function getCity(event) {
+        setCity(event.target.value);
+    }
+
+    function getCountry(event) {
+        setCountry(event.target.value);
+    }
+
+    function handleInput() {
+
+    }
+
     return (
         <div className={className}>
             <label>
                 Enter your location
                 <br />
-                <input type='text' placeholder='City'/>
+                <input type='text' onChange={() => getCity(e)} placeholder='City'/>
                 <br />
-                <input type='text' placeholder='Country' />
+                <input type='text'  onChange={() => getCountry(e)}  placeholder='Country' />
+                <br />
+                <button onClick={() => handleInput()}>Submit</button>
             </label>
         </div>
     )
